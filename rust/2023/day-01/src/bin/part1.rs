bf05eb83fdec8fa5bf96afa3b26f5d52a062b89b
ChristianAnagnostou <christian0722@gmail.com>
2023-12-02 11:48:36 -0800

    solve day-01

--- rust/2023/day-01/src/bin/part1.rs
+++ rust/2023/day-01/src/bin/part1.rs
@@ -1,35 +1,13 @@
 fn main() {
     let input = include_str!("../puzzle-input.txt");
-    let output = sum_calibrations(input);
+    let output: usize = input.lines().map(parse_calibration_value).sum();
     println!("Output: {}", output);
 }
 
-fn sum_calibrations(input: &str) -> i32 {
-    let mut sum = 0;
-    for line in input.lines() {
-        if line.is_empty() {
-            continue;
-        }
-        let value = parse_calibration_value(&line).unwrap_or(0);
-        sum += value;
-    }
-    return sum;
-}
-
-fn parse_calibration_value(line: &str) -> Option<i32> {
-    let first_digit = line.chars().find(|c| c.is_digit(10));
-    let last_digit = line.chars().rfind(|c| c.is_digit(10));
-
-    match (first_digit, last_digit) {
-        (Some(first), Some(last)) => {
-            let concatenated = format!("{}{}", first, last);
-            Some(concatenated.parse().unwrap())
-        }
-        _ => {
-            println!("No digits found");
-            None
-        }
-    }
+fn parse_calibration_value(line: &str) -> usize {
+    let first_digit = line.chars().find(|c| c.is_numeric()).unwrap();
+    let last_digit = line.chars().rfind(|c| c.is_numeric()).unwrap();
+    format!("{first_digit}{last_digit}").parse().unwrap()
 }
 
 #[cfg(test)]
@@ -37,13 +15,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = sum_calibrations(
-            "1abc2
-            pqr3stu8vwx
-            a1b2c3d4e5f
-            treb7uchet",
-        );
+    fn part1_works() {
+        let input = "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet";
+
+        let result: usize = input.lines().map(parse_calibration_value).sum();
         assert_eq!(result, 142);
     }
 }
