32f3380c6a072af1042dc950dda43409bbb50f68
ChristianAnagnostou <christian0722@gmail.com>
2023-12-02 19:38:41 -0800

    Fix typo

--- rust/2023/day-02/src/bin/part2.rs
+++ rust/2023/day-02/src/bin/part2.rs
@@ -34,7 +34,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1_works() {
+    fn part2_works() {
         let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
         Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
         Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
